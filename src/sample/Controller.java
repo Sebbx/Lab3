b4abc27a7984981f552b7af5c481995fb38bac06
Sebbx <56113951+Sebbx@users.noreply.github.com>
2021-04-24 20:13:29 +0200

    Lab 3| 6. Kalkulator wagi roaz ceny materiału z wymiarów walca

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,22 +1,51 @@
 package sample;
-
 import javafx.event.ActionEvent;
-import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
 public class Controller {
-    public Button btnMain;
-    public TextField txtLicznik;
-    int clickCounter = 0;
+    public TextField txtLicznik1, txtLicznik2, txtLicznik3, txtWagaNorm, txtWagaNierdz, txtCenaNorm, txtCenaNierdz;
 
-    public void onBtnAction(ActionEvent actionEvent)
+    public void OnTxtAction(ActionEvent actionEvent)
     {
-        System.out.println("Kliknięcie nr " + ++clickCounter);
-        if (clickCounter == 1)
-        {
-            btnMain.setText("I jeszcze raz");
-        }
-        txtLicznik.setText(String.valueOf(clickCounter));
+        double gestoscStali = 750;
+        double gestoscStaliNierdzewnej = 786;
+
+        double cenaStali = 0;
+        double cenaStaliNierdz = 0;
+
+        float promien = Float.parseFloat(txtLicznik1.getText());
+        float wysokosc = Float.parseFloat(txtLicznik2.getText());
+        float gruboscBlachy = Float.parseFloat(txtLicznik3.getText());
+
+        double poleCalkowiteWalca = 2 * Math.PI * (promien * promien) + 2 * Math.PI * promien * wysokosc;
+
+        double dlugosciBokowBlachy = Math.sqrt(poleCalkowiteWalca);
+
+        double objetoscBlachy = dlugosciBokowBlachy * dlugosciBokowBlachy * gruboscBlachy;
+
+        double wagaBlachyZeStali = objetoscBlachy * gestoscStali;
+        double wagaBlachyZeStaliNierdzewnej = objetoscBlachy * gestoscStaliNierdzewnej;
+
+        wagaBlachyZeStali = wagaBlachyZeStali / 1000;
+        wagaBlachyZeStaliNierdzewnej = wagaBlachyZeStaliNierdzewnej / 1000;
+
+        cenaStali = wagaBlachyZeStali * 2.90;
+        cenaStaliNierdz = wagaBlachyZeStaliNierdzewnej * 5.90;
+
+        txtWagaNorm.setText(String.valueOf(wagaBlachyZeStali));
+        txtWagaNierdz.setText(String.valueOf(wagaBlachyZeStaliNierdzewnej));
+
+        txtCenaNorm.setText(String.valueOf(cenaStali));
+        txtCenaNierdz.setText(String.valueOf(cenaStaliNierdz));
+
+
+
+
+
+
+
+
+
     }
 
 }
